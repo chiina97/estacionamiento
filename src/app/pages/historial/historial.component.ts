5813af4b5217205c64747f38a2a2f047b4055d19
yajaira <yajairaquipusco97@gmail.com>
2022-01-18 12:48:32 -0300

    limpiando codigo

--- src/app/pages/historial/historial.component.ts
+++ src/app/pages/historial/historial.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Historial } from 'src/app/models/historial';
-import { Usuario } from 'src/app/models/usuario';
 import { HistorialService } from 'src/app/service/historial.service';
 import { TokenService } from 'src/app/service/token.service';
-import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
   selector: 'app-historial',
@@ -11,16 +9,13 @@ import { UsuarioService } from 'src/app/service/usuario.service';
 })
 export class HistorialComponent implements OnInit {
   userId!:number;
-  idCuenta!:number;
   historial:Historial[]=[];
 
    //paginado:
    totalLength:any;
    page:number=1;
-   today = new Date();
 
   constructor(private historialService:HistorialService,
-    private usuarioService:UsuarioService,
     private tokenService:TokenService,) {
       this.userId=Number(this.tokenService.getIdUser());
      }
